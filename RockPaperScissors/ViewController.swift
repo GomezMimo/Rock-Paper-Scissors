1e51a1fbf0221c3d84ca250bce8762049f8f84fe
Juan Gomez <juan.camilo.mimo@gmail.com>
2015-07-27 11:16:27 -0500

    Rock Paper Scissors using shake motion

--- RockPaperScissors/ViewController.swift
+++ RockPaperScissors/ViewController.swift
@@ -7,12 +7,29 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var rps_image: UIImageView!
+    var options = ["rock.jpg", "paper.jpg", "scissors.jpg"]
+    var image :String = String()
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        super.viewDidLoad()        
+    }
+    
+    func shakeGame(){
+        var random = Int(arc4random_uniform(UInt32(options.count)))
+        image = options[random]
+    }
+    
+    
+    
+    override func motionEnded(motion: UIEventSubtype, withEvent event: UIEvent) {
+        if event.subtype == UIEventSubtype.MotionShake{
+            shakeGame()
+            rps_image.image = UIImage(named: image)
+        }
     }
 
     override func didReceiveMemoryWarning() {
